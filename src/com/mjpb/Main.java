21d068585bc44ae67df619608a7393985e9979e1
Mariah Baysic <mjpb123@gmail.com>
2019-02-11 23:36:14 +0800

    SumOddRange.java - determines if a number is odd, returns the sum of all odd numbers in a given range

--- src/com/mjpb/Main.java
+++ src/com/mjpb/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
-        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+        System.out.println(SumOddRange.sumOdd(100,1000));
+
+//        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
+//        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
 
 //        NumberInWord.printNumberInWord(0);
 
---
+++ src/com/mjpb/SumOddRange.java
@@ -0,0 +1,28 @@
+package com.mjpb;
+
+public class SumOddRange {
+
+    public static boolean isOdd(int number) {
+        if (number < 1)
+            return false;
+
+        return number % 2 != 0;
+    }
+
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+
+        if (end < start || end < 1 || start < 1)
+            return -1;
+
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i))
+                sum += i;
+        }
+
+        return sum;
+
+    }
+
+}

d11767dd5f9a011503d6d8e722a0cd541318fcca
Mariah Baysic <mjpb123@gmail.com>
2019-02-11 23:20:34 +0800

    NumberOfDaysInMonth.java - determines if year is a leap year | returns the number of days in a given month within a given year

--- src/com/mjpb/Main.java
+++ src/com/mjpb/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        NumberInWord.printNumberInWord(0);
+        System.out.println(NumberOfDaysInMonth.isLeapYear(1600));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+
+//        NumberInWord.printNumberInWord(0);
 
 //        DayOfTheWeek.switchDayOfTheWeek(7);
 //        DayOfTheWeek.ifDayOfTheWeek(7);
